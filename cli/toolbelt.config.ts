c9c9ef2f10ec80c7cd16356ba4c77b4b3e2f44b3
Ron Parnaso <rolme@Rons-MacBook-Pro.local>
2019-10-12 06:16:23 -0700

    Switch from exports.yml to config.yml

--- cli/toolbelt.config.ts
+++ cli/toolbelt.config.ts
@@ -2,5 +2,5 @@ import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
 export const Config: { [key: string]: any } = yaml.safeLoad(
-  fs.readFileSync(__dirname + '/../exports.yml', 'utf8'),
+  fs.readFileSync(__dirname + '/../config.yml', 'utf8'),
 ) as any;
